2f69c46354b968017db361566c2d7181a38151b8
skymoneyc <CChain0615@gmail.com>
2016-03-26 20:59:41 +0800

    update

--- site_qr_share/popup.js
+++ site_qr_share/popup.js
@@ -2,4 +2,25 @@
 * Site share Ext
 */
 
-console.log("site share");
+chrome.tabs.query({'active': true}, function(tabs){
+  var url = tabs[0].url;
+  var target_url = "http://tool.oschina.net/action/qrcode/generate";
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    url: target_url,
+    crossDomain: true,
+    headers: {},
+    data: {"data": url,
+    "output": "image/gif",
+    "error": "L",
+    "type": 0,
+    "margin": 0,
+    "size": 4}}).done(function(data){
+      alert("down");
+    })
+    .fail(function(xhr, text){
+      alert("error");
+    });
+
+});
